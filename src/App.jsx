04c14b4981ddd34d51c217e53c477736cb3babd0
emitaylor <letailleur.emilie@gmail.com>
2024-03-24 17:39:44 +0100

    sort and show countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import { useState, useEffect } from "react"
+import ListCard from "./components/ListCard"
 
 function App() {
   const [countries, setCountries] = useState()
-  
+
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/region/europe")
     .then(data => data.json())
-    .then(data => console.log(data))
+    .then(data => {
+      console.log(data)
+      data.sort((a,b) => {
+        if(a.name.common < b.name.common) {
+          return -1
+        }
+        else if(a.name.common > b.name.common) {
+          return 1
+        }
+        else {
+          return 0
+        }
+    })
+    setCountries(data)
+    })
   }, [])
 
   return (
@@ -14,6 +29,13 @@ function App() {
       <div className="max-w-7xl mx-auto py-20 px-4">
         <h1 className="text-gray-50 text-4xl">Europe Countries Datas</h1>
         <p className="text-gray-100 text-xl mb-8">Click on a card to reveal the country's informations</p>
+        {countries && (
+          <ul className="grid min-[450px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 auto-rows-[200px]">
+            {countries.map((country, index) => (
+              <ListCard key={index} country={country} />
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   )
